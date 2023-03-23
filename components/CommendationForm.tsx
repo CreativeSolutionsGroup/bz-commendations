597406e6a3cfd84c182b99455654a9e3b4ed5d1f
Alex Taylor <52684858+awtpi314@users.noreply.github.com>
2023-03-23 09:39:56 -0400

    added faster autocomplete

--- components/CommendationForm.tsx
+++ components/CommendationForm.tsx
@@ -1,17 +1,137 @@
-import { Button, Fab, TextField, Typography, MenuItem, Stack, Avatar, Paper, Autocomplete, Snackbar, Alert } from "@mui/material"
-import { Box } from "@mui/system"
-import { SyntheticEvent, useState } from "react"
-import { Member, Team } from "@prisma/client";
-import SendIcon from "@mui/icons-material/Send"
+import bz from "@/assets/bz-logo.png";
+import SendIcon from "@mui/icons-material/Send";
+import { Autocomplete, Avatar, Button, ListSubheader, MenuItem, Paper, Popper, Stack, TextField, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { autocompleteClasses } from "@mui/material/Autocomplete";
+import { Box } from "@mui/system";
 import { Raleway } from "@next/font/google";
+import { Member, Team } from "@prisma/client";
 import Image from "next/image";
-import bz from "@/assets/bz-logo.png"
+import { createContext, forwardRef, HTMLAttributes, ReactNode, useContext, useEffect, useRef, useState } from "react";
+import { ListChildComponentProps, VariableSizeList } from "react-window";
 
 const raleway = Raleway({ subsets: ["latin"], weight: "900" });
+
+const LISTBOX_PADDING = 8;
+
+function renderRow(props: ListChildComponentProps) {
+  const { data, index, style } = props;
+  const dataSet = data[index];
+  const inlineStyle = {
+    ...style,
+    top: (style.top as number) + LISTBOX_PADDING,
+  };
+
+  if (dataSet.hasOwnProperty("group")) {
+    return (
+      <ListSubheader key={dataSet.key} component="div" style={{ ...inlineStyle, height: 36}}>
+        {dataSet.group}
+      </ListSubheader>
+    );
+  }
+
+  const [, member] = dataSet;
+
+  return (
+    <MenuItem key={index} {...props} sx={{ width: "100%" }}>
+      <Box display="flex" flexDirection="row" width="100%">
+        <Avatar>
+          <Image fill src={member.imageURL ?? "https://via.placeholder.com/25?text="} alt="" placeholder="blur" blurDataURL={bz.src} />
+        </Avatar>
+        <Typography ml={1.5} mt={1}>{member.name}</Typography>
+        <Box flexGrow={1}></Box>
+        <Typography mt={1.5} variant="caption" color="GrayText" align="right" maxWidth='10rem' overflow="hidden">
+          {member.team.map((team: Team) => team.name).join(", ")}
+        </Typography>
+      </Box>
+    </MenuItem>
+  )
+}
+
+const OuterElementContext = createContext({});
+
+const OuterElementType = forwardRef<HTMLDivElement>((props, ref) => {
+  const outerProps = useContext(OuterElementContext);
+  return <div ref={ref} {...props} {...outerProps} />
+})
+
+function useResetCache(data: any) {
+  const ref = useRef<VariableSizeList>(null);
+  useEffect(() => {
+    if (ref.current != null) {
+      ref.current.resetAfterIndex(0, true);
+    }
+  }, [data]);
+  return ref;
+}
+
+const ListboxComponent = forwardRef<
+  HTMLDivElement,
+  HTMLAttributes<HTMLElement>
+>(function ListboxComponent(props, ref) {
+  const { children, ...other } = props;
+  const itemData: ReactNode[] = [];
+  (children as (ReactNode & { children?: ReactNode[] })[]).forEach((item) => {
+    itemData.push(item);
+    itemData.push(...(item.children || []));
+  });
+
+  const itemCount = itemData.length;
+  const itemSize = 48;
+
+  const getChildSize = (child: ReactNode) => {
+    if (child?.hasOwnProperty("group")) {
+      return 48;
+    }
+
+    return itemSize;
+  };
+
+  const getHeight = () => {
+    if (itemCount > 8) {
+      return 8 * itemSize;
+    }
+    return itemData.map(getChildSize).reduce((a, b) => a + b, 0);
+  };
+
+  const gridRef = useResetCache(itemCount);
+
+  return (
+    <div ref={ref}>
+      <OuterElementContext.Provider value={other}>
+        <VariableSizeList
+          itemData={itemData}
+          height={getHeight() + 2 * LISTBOX_PADDING}
+          width="100%"
+          ref={gridRef}
+          outerElementType={OuterElementType}
+          innerElementType="ul"
+          itemSize={(index) => getChildSize(itemData[index])}
+          overscanCount={5}
+          itemCount={itemCount}
+        >
+          {renderRow}
+        </VariableSizeList>
+      </OuterElementContext.Provider>
+    </div>
+  );
+});
+
+const StyledPopper = styled(Popper)({
+  [`& .${autocompleteClasses.listbox}`]: {
+    boxSizing: "border-box"
+    ,
+    "& ul": {
+      padding: 0,
+      margin: 0
+    }
+  }
+})
 
 export default ({ members }: { members: Array<Member & { team: Array<Team> }> }) => {
   const [sending, setSending] = useState(false);
   const [memberData, setToMember] = useState("");
+
 
 
   return (
@@ -22,24 +142,20 @@ export default ({ members }: { members: Array<Member & { team: Array<Team> }> })
           <TextField sx={{ display: "none" }} hidden name="recipient" value={memberData} />
           <Autocomplete
             onChange={(_e, v) => setToMember(v?.id ?? "")}
-            renderOption={(props, member, state) =>
-              <MenuItem key={state.index} {...props} sx={{ width: "100%" }}>
-                <Box display="flex" flexDirection="row" width="100%">
-                  <Avatar>
-                    <Image fill src={member.imageURL ?? "https://via.placeholder.com/25?text="} alt="" placeholder="blur" blurDataURL={bz.src} />
-                  </Avatar>
-                  <Typography ml={1.5} mt={1}>{member.name}</Typography>
-                  <Box flexGrow={1}></Box>
-                  <Typography mt={1.5} variant="caption" color="GrayText" align="right" maxWidth='10rem' overflow="hidden">
-                    {member.team.map((team) => team.name).join(", ")}
-                  </Typography>
-                </Box>
-              </MenuItem>}
+            id="virtualize-commendation"
+            sx={{ width: "100%" }}
+            disableListWrap
+            PopperComponent={StyledPopper}
+            ListboxComponent={ListboxComponent}
+            options={members}
             getOptionLabel={(member) => member.name}
-            options={members} groupBy={(member) => member.name.charAt(0)}
-            renderInput={(params) => <TextField {...params}
-              label="To"
-              required />} />
+            groupBy={(option) => option.name[0].toUpperCase()}
+            renderInput={(params) => <TextField {...params} label="To *" />}
+            renderOption={(props, option, state) =>
+              [props, option, state.index] as ReactNode
+            }
+            renderGroup={(params) => params as unknown as ReactNode}
+          />
           <TextField required label="Message" variant="filled" name="msg" minRows={8} multiline={true} />
           <Button disabled={sending} variant="contained" color="secondary" type="submit" endIcon={<SendIcon />} sx={{ fontSize: 18, textTransform: "uppercase", minWidth: "fit-content" }}>
             Send

3bfa0d890c4292999d52383bea9e1c090be990c5
Alex Taylor <52684858+awtpi314@users.noreply.github.com>
2023-04-24 08:04:54 -0400

    changed name of function

--- pages/api/user/teams.ts
+++ pages/api/user/teams.ts
@@ -7,7 +7,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
  * @param req the API request from next
  * @param res the API response that we send back
  */
-export default async function SetDateCookie(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { email } = req.query;
   const member = await getMemberWithTeams(await emailToId(email as string) ?? "");
 

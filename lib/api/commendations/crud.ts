d975e256d8c3991918b01822b99c06fca7ab25fd
Konolo <wjacobs@cedarville.edu>
2023-06-28 19:33:00 -0400

    Some of the crud functions are documented

--- lib/api/commendations/crud.ts
+++ lib/api/commendations/crud.ts
@@ -1,6 +1,13 @@
 import { prisma } from "@/lib/api/db";
 import { Member } from "@prisma/client";
 
+/**
+ * THIS FUNCTION DOES NOT APPEAR TO BE USED ANYWHERE WITHIN THE PROGRAM
+ * 
+ * This function returns the name of the student whose id is provided
+ * @param studentId A string which represents the id associated with a student
+ * @returns The name associated with the provided id as a string or an empty string if the id is not found
+ */
 export const idToName = async (studentId: string) => {
   const student = await prisma.member.findFirst({ where: { id: studentId } });
 
@@ -13,6 +20,12 @@ export const idToName = async (studentId: string) => {
   return name as string;
 };
 
+
+/**
+ * This gets the id of the member whose email was provided
+ * @param sender An email address of the person you want the id for. This it is a string
+ * @returns The id of the member with the provided email address or nothing if no member is found
+ */
 export const emailToId = async (sender: string) => {
   const member = await prisma.member.findFirst({ where: { email: sender } });
 
@@ -25,6 +38,14 @@ export const emailToId = async (sender: string) => {
   return id as string;
 };
 
+
+/**
+ * 
+ * @param sender 
+ * @param recipient 
+ * @param msg 
+ * @returns 
+ */
 export const createCommendation = async (
   sender: string, recipient: string, msg: string
 ) => {
@@ -45,10 +66,21 @@ export const createCommendation = async (
   });
 };
 
+
+/**
+ * This gets all of the Commendations that have been sent
+ * @returns A list of all Commendations in `Commendation[]` format
+ */
 export const readAllCommendations = async () => {
   return await prisma.commendation.findMany();
 };
 
+
+/**
+ * 
+ * @param currentUserEmail 
+ * @returns 
+ */
 export const readAllMembers = async (currentUserEmail = "") => {
   return await prisma.member.findMany({
     where: {
@@ -65,6 +97,13 @@ export const readAllMembers = async (currentUserEmail = "") => {
   });
 };
 
+
+/**
+ * 
+ * @param image 
+ * @param id 
+ * @returns 
+ */
 export const updateMemberImageURL = async (image: string, id: string) => {
   return await prisma.member.update({
     data: {
@@ -76,6 +115,12 @@ export const updateMemberImageURL = async (image: string, id: string) => {
   });
 };
 
+
+/**
+ * 
+ * @param email 
+ * @returns 
+ */
 export const readUserCommendations = async (email: string) => {
   const user = await prisma.member.findFirst({
     select: {
@@ -98,6 +143,12 @@ export const readUserCommendations = async (email: string) => {
   return user?.commendations;
 };
 
+
+/**
+ * 
+ * @param email 
+ * @returns 
+ */
 export const readUserSentCommendations = async (email: string) => {
   const user = await prisma.member.findFirst({
     select: {
@@ -120,6 +171,7 @@ export const readUserSentCommendations = async (email: string) => {
   return user?.sentCommendations;
 };
 
+
 /**
  * The function name here is misleading.
  * This gets all of the team leaders for a given team.
@@ -141,6 +193,12 @@ export const getMemberTeamLeaders = async (teams: string[]) => {
   return teamLeaders.map(l => l.member);
 };
 
+
+/**
+ * 
+ * @param id 
+ * @returns 
+ */
 export const getMemberWithTeams = async (id: string) => {
   return await prisma.member.findFirst({
     where: {
@@ -152,6 +210,12 @@ export const getMemberWithTeams = async (id: string) => {
   });
 };
 
+
+/**
+ * 
+ * @param id 
+ * @returns 
+ */
 export const idIsMember = async (id: string) => {
   return !!await prisma.member.count({
     where: {
@@ -160,6 +224,12 @@ export const idIsMember = async (id: string) => {
   });
 };
 
+
+/**
+ * 
+ * @param id 
+ * @returns 
+ */
 export const getMemberImage = async (id: string) => {
   const image = await prisma.member.findFirst({
     select: {

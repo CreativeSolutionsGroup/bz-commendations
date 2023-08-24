a60c3cb2d549c48c4a33b9dff75e73958f96267e
Ashlyn DeVries <ashjdevries@gmail.com>
2023-08-24 16:36:36 -0400

    Centered text

--- components/NotFound.tsx
+++ components/NotFound.tsx
@@ -6,7 +6,7 @@ const raleway = Raleway({ subsets: ["latin"], weight: "900" });
 export function NotFound({ page }: { page: string }) {
   return (
     <Paper sx={{ mt: 4, mx: "auto", maxWidth: "30rem", p: 2 }}>
-      <Typography color="primary" className={raleway.className} fontSize={25} fontWeight={900} sx={{ mx: "auto", my: "auto" }}>
+      <Typography color="primary" className={raleway.className} fontSize={25} fontWeight={900} align="center">
         You have {page} zero commendations.
       </Typography>
     </Paper>

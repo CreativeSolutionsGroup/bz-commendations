72c03f988dd3ebb09dbee119805199e5e5fa7206
Konolo <wjacobs@cedarville.edu>
2023-09-25 20:06:33 -0400

    Added the drawer and moved the pages over

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -4,7 +4,7 @@ import AdminSquareView from "@/components/AdminSquareView";
 import AdminTeamView from "@/components/AdminTeamView";
 import { TimeRangeCommendations } from "@/types/commendation";
 import { EmojiEvents, GridView, Group, Settings } from "@mui/icons-material";
-import { Button, Card, IconButton, Menu, MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import { Button, Card, Divider, Drawer, IconButton, List, ListItem, ListItemButton, Menu, MenuItem, Select, SelectChangeEvent } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import { Box } from "@mui/system";
 import { DatePicker } from "@mui/x-date-pickers";
@@ -48,111 +48,115 @@ export default function Admin() {
     return b ? b.pop() : "";
   }
 
+  const drawerWidth = 260;
+
   return (
-    <>
-      <Box display={"flex"} flexDirection={"row"} flexWrap={"wrap"}>
-        <Typography
-          sx={{
-            textAlign: "center",
-            fontSize: 30,
-            fontWeight: 900,
-            marginTop: 1,
-            flexGrow: 1,
-            className: raleway.className            
-          }}
-        >
+    <Box display={"flex"} flexDirection={"row"}>
+      <Box width={drawerWidth}>
+        <Drawer sx={{ width: drawerWidth, flexShrink: 0, "& .MuiDrawer-paper": { position: "relative", borderColor: "white", width: drawerWidth, boxSizing: "border-box", }, }} variant="permanent" anchor="left">
+          <List>
+            <ListItemButton onClick={() => { setViewMode("overview"); }}>
+              <Box display={"flex"} flexDirection={"row"}>
+                <Typography ml={1} fontWeight="bold">Overview</Typography>
+              </Box>
+            </ListItemButton>
+            <ListItemButton onClick={() => { setViewMode("square"); }}>
+              <Box display={"flex"} flexDirection={"row"}>
+                <GridView />
+                <Typography ml={1} fontWeight="bold">Square</Typography>
+              </Box>
+            </ListItemButton>
+            <ListItemButton onClick={() => { setViewMode("memberLeaderboard"); }}>
+              <Box display={"flex"} flexDirection={"row"}>
+                <EmojiEvents />
+                <Typography ml={1} fontWeight={700}>Member Leaderboard</Typography>
+              </Box>
+            </ListItemButton>
+            <ListItemButton onClick={() => { setViewMode("teamLeaderboard"); }}>
+              <Box display={"flex"} flexDirection={"row"}>
+                <EmojiEvents />
+                <Typography ml={1} fontWeight={700}>Team Leaderboard</Typography>
+              </Box>
+            </ListItemButton>
+            <ListItemButton onClick={() => { setViewMode("teams"); }}>
+              <Box display={"flex"} flexDirection={"row"}>
+                <Group />
+                <Typography ml={1} fontWeight={700}>Teams</Typography>
+              </Box>
+            </ListItemButton>
+          </List>
+          <Divider />
+          <List>
+            <Box sx={{ display: "flex", alignItems: "center", marginRight: 2, marginTop: 1, marginLeft: 2 }}>
+              <Box flexGrow={1} />
+              <Typography mr={1}>Start</Typography>
+              <DatePicker
+                value={firstDate}
+                onChange={v => {
+                  v && setFirstDate(v);
+                  v && fetch(`/api/admin?first=${v}`, { method: "POST" });
+                  handleSettingsClose();
+                }}
+                sx={{ width: 175, marginRight: 1 }}
+              />
+            </Box>
+            <Box sx={{ display: "flex", alignItems: "center", marginRight: 2, marginTop: 1 }}>
+              <Box flexGrow={1} />
+              <Typography mr={1}>End</Typography>
+              <DatePicker
+                value={secondDate}
+                onChange={v => {
+                  v && setSecondDate(v);
+                  v && fetch(`/api/admin?second=${v}`, { method: "POST" });
+                  handleSettingsClose();
+                }}
+                sx={{ width: 175, marginRight: 1 }}
+              />
+            </Box>
+            <Button
+              sx={{ float: "right", marginTop: 1, marginRight: 3, marginBottom: 2 }}
+              onClick={() => {
+                const first = dayjs().set("date", 1).set("hour", 1);
+                const second = dayjs();
+                setFirstDate(first);
+                fetch(`/api/admin?first=${first}`, { method: "POST" });
+                setSecondDate(second);
+                fetch(`/api/admin?second=${second}`, { method: "POST" });
+                handleSettingsClose();
+              }}> 
+              <Typography color={theme.palette.primary.main} fontWeight="bold" textTransform="capitalize">Reset Date Range</Typography>
+            </Button>
+          </List>
+        </Drawer>
+      </Box>
+      <Box width={`calc(100% - ${drawerWidth}px)`}>
+        <Box display={"flex"} flexDirection={"row"} flexWrap={"wrap"}>
+          <Typography
+            sx={{
+              textAlign: "center",
+              fontSize: 30,
+              fontWeight: 900,
+              marginTop: 1,
+              flexGrow: 1,
+              className: raleway.className            
+            }}
+          >
           Admin Dashboard
-        </Typography>
-        <Menu
-          anchorEl={settingsAnchorEl}
-          open={settingsOpen}
-          onClose={handleSettingsClose}
-        >
-          <Box sx={{ display: "flex", alignItems: "center", mr: 2, pl: 2 }}>
-            <Box flexGrow={1} />
-            <Typography mr={1}>View</Typography>
-            <Select label="View" name="view" value={viewMode} onChange={(e: SelectChangeEvent) => { setViewMode(e.target.value); handleSettingsClose(); }} sx={{ marginRight: 1 }}>
-              <MenuItem key={1} value={"square"}>
-                <Box display={"flex"} flexDirection={"row"}>
-                  <GridView />
-                  <Typography ml={1} fontWeight="bold">Square</Typography>
-                </Box>
-              </MenuItem>
-              <MenuItem key={2} value={"memberLeaderboard"}>
-                <Box display={"flex"} flexDirection={"row"}>
-                  <EmojiEvents />
-                  <Typography ml={1} fontWeight={700}>Member Leaderboard</Typography>
-                </Box>
-              </MenuItem>
-              <MenuItem key={3} value={"teamLeaderboard"}>
-                <Box display={"flex"} flexDirection={"row"}>
-                  <EmojiEvents />
-                  <Typography ml={1} fontWeight={700}>Team Leaderboard</Typography>
-                </Box>
-              </MenuItem>
-              <MenuItem key={4} value={"teams"}>
-                <Box display={"flex"} flexDirection={"row"}>
-                  <Group />
-                  <Typography ml={1} fontWeight={700}>Teams</Typography>
-                </Box>
-              </MenuItem>
-            </Select>
-          </Box>
-          <Box sx={{ display: "flex", alignItems: "center", marginRight: 2, marginTop: 1, marginLeft: 2 }}>
-            <Box flexGrow={1} />
-            <Typography mr={1}>Start</Typography>
-            <DatePicker
-              value={firstDate}
-              onChange={v => {
-                v && setFirstDate(v);
-                v && fetch(`/api/admin?first=${v}`, { method: "POST" });
-                handleSettingsClose();
-              }}
-              sx={{ width: 175, marginRight: 1 }}
-            />
-          </Box>
-          <Box sx={{ display: "flex", alignItems: "center", marginRight: 2, marginTop: 1 }}>
-            <Box flexGrow={1} />
-            <Typography mr={1}>End</Typography>
-            <DatePicker
-              value={secondDate}
-              onChange={v => {
-                v && setSecondDate(v);
-                v && fetch(`/api/admin?second=${v}`, { method: "POST" });
-                handleSettingsClose();
-              }}
-              sx={{ width: 175, marginRight: 1 }}
-            />
-          </Box>
-          <Button
-            sx={{ float: "right", marginTop: 1, marginRight: 3, marginBottom: 2 }}
-            onClick={() => {
-              const first = dayjs().set("date", 1).set("hour", 1);
-              const second = dayjs();
-              setFirstDate(first);
-              fetch(`/api/admin?first=${first}`, { method: "POST" });
-              setSecondDate(second);
-              fetch(`/api/admin?second=${second}`, { method: "POST" });
-              handleSettingsClose();
-            }}> 
-            <Typography color={theme.palette.primary.main} fontWeight="bold" textTransform="capitalize">Reset Date Range</Typography>
-          </Button>
-        </Menu>
-        <IconButton onClick={handleSettingsClick} sx={{ position: "absolute", right: 6, top: 70 }}>
-          <Settings sx={{ marginY: "auto" }}></Settings>
-        </IconButton>
+          </Typography>
+        
+        </Box>
+        {viewMode === "square" ?
+          <AdminSquareView teams={data.teams} /> : viewMode === "memberLeaderboard" ?
+            <AdminMemberLeaderboardView members={data.members} /> : viewMode === "teamLeaderboard" ?
+              <AdminTeamLeaderboardView teams={data.teams} /> :
+              <AdminTeamView teams={data.teams}/>
+        }
+        <Box sx={{ position: "fixed", bottom: 0, display: "flex" }}>
+          <Card sx={{ marginLeft: 1, marginBottom: 1, fontSize: 20, padding: 1 }}>
+            <Typography color={theme.palette.primary.main} fontWeight="bold">Commendation Count: {data.members?.sendMembers.reduce((prev, curr) => prev + curr.sentCommendations.length, 0)}</Typography>
+          </Card>
+        </Box>
       </Box>
-      {viewMode === "square" ?
-        <AdminSquareView teams={data.teams} /> : viewMode === "memberLeaderboard" ?
-          <AdminMemberLeaderboardView members={data.members} /> : viewMode === "teamLeaderboard" ?
-            <AdminTeamLeaderboardView teams={data.teams} /> :
-            <AdminTeamView teams={data.teams}/>
-      }
-      <Box sx={{ position: "fixed", bottom: 0, display: "flex" }}>
-        <Card sx={{ marginLeft: 1, marginBottom: 1, fontSize: 20, padding: 1 }}>
-          <Typography color={theme.palette.primary.main} fontWeight="bold">Commendation Count: {data.members?.sendMembers.reduce((prev, curr) => prev + curr.sentCommendations.length, 0)}</Typography>
-        </Card>
-      </Box>
-    </>
+    </Box>
   );
 }

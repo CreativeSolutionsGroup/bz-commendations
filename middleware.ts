85d8941dfe2a362d3eeee8881b3c4c1bca64de95
Alexander Taylor <52684858+awtpi314@users.noreply.github.com>
2024-10-01 16:39:33 -0400

    Update middleware.ts
    Fixed issue with seeing everybody's commendations

--- middleware.ts
+++ middleware.ts
@@ -13,6 +13,13 @@ export default withAuth({
         return token?.isAdmin ?? false;
       }
 
+      if (pathname.includes("/me")) {
+        const email = searchParams.get("email");
+        // fix for #107
+        if (email == null) return true;
+        return email === token?.email;
+      }
+
       // otherwise, if you were allowed to login, you're good.
       return !!token;
     },

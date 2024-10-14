ff149694b8d8cca31ee42efdd9a68969698e2dfb
Zaine F <48459250+ZGeek03@users.noreply.github.com>
2024-10-14 17:23:05 -0400

    Switched auth to Azure AD

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,29 +1,17 @@
 import { Paper, Stack, Typography, Button } from "@mui/material";
 import { signIn } from "next-auth/react";
 import { Raleway } from "@next/font/google";
-import GoogleIcon from "@/components/GoogleIcon";
+import MicrosoftIcon from "@/components/MicrosoftIcon";
 
 const raleway = Raleway({ subsets: ["latin"], weight: "900" });
 
 export default function SignIn() {
   return (
-    <Paper
-      elevation={4}
-      sx={{ mt: "15rem", mx: "auto", maxWidth: "18rem", py: 1 }}
-    >
-      <Stack alignItems="center">
-        <Button onClick={() => signIn("google", { callbackUrl: "/" })}>
-          <GoogleIcon />
-          <Typography
-            className={raleway.className}
-            fontSize={18}
-            fontWeight={900}
-            sx={{ ml: 1.5 }}
-          >
-            Sign in with Google
-          </Typography>
-        </Button>
-      </Stack>
-    </Paper>
+  
+    <Stack alignItems="center" justifyContent="center" sx={{mx: "auto", height: "85vh"}}>
+      <Button onClick={() => signIn("azure-ad", { callbackUrl: "/" })}>
+        <MicrosoftIcon />
+      </Button>
+    </Stack>
   );
 }
